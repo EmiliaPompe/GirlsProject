98955dc3888aa1424f3945b59e633f7537fd6879
Virginia Aglietti <aglietti@toucanet.stats.ox.ac.uk>
2016-11-24 17:30:15 +0000

    modify MH function

--- general_MH_function.R
+++ general_MH_function.R
@@ -9,39 +9,40 @@ prop_par = c(0.1)   #in this case my proposed value is sigma
 
 
 
-MH <- function(data, d = 1, n_iter = 1000, initial_value, prior_par, target_distribution, proposal_distribution, prop_par, ...){ #pass the proposed values to the proposal distribution
+MH <- function(data, d = 1, n_iter = 1000, initial_value, prior_par, proposal_distribution, prop_par, target_distribution, ...){ #pass the proposed values to the proposal distribution
   # MH is an implementation of an M-H algorithm
-  # the posterior (target distribution) can be a Beta, a univariate Gaussian
-  # the proposal distribution is passed in the function together with the pars to pass
-  # BetaMH args:  data         - numeric vector of data
-  #               N            - number of iterations
-  #               x_0          - starting point of our Markov chain   
-  # alpha_prior & beta_prior   - parameters of the prior Beta distribution
-  #               sigma        - sd of the proposal (Normal) distribution
+  # data          - vector of data
+  # n_iter        - number of iterations
+  # initial_value - starting point of our Markov chain   
+  # prior_par     - vector or list of prior parameters for that need to be passed to the target distr. to update the pars
+  # proposal d.   - distribution used to proposed a new value together with prop_par
+  # prop_par      - value of the parameters to be passed to the proposal distribution
+  # target d.     - posterior distribution can be a Beta, a univariate Gaussian dist or a multivariate Gaussian
+  # ...           - use to pass the value of known par to the normal posterior distribution
+  
   
   acc <- 0
-  x <- matrix(NA, nrow=n_iter+1, ncol=d)  #matrix (vector if d=1) to store the values of the Markov Chain
-  x[1,] <- initial_value                  #store initial value. The first row stores the proposed values.  
-  #nr_successes <- sum(data==1)
-  #nr_trials <- length(data)
+  x <- matrix(NA, nrow=n_iter+1, ncol=d)  # matrix (vector if d=1) to store the values of the Markov Chain
+  x[1,] <- initial_value                  # store initial value in the first row of x  
+
   for (i in seq_len(n_iter)){
     
     # we propose a value sampled from the proposal_distribution with par = prop_par
-    #x_proposed <- rnorm(1, mean = x[i], sd = sigma)
-    x_proposed = proposal_distribution(d, prop_par)
+    x_proposed = proposal_distribution(prop_par)
     
-    #when we assume a symmetric proposal distribution the accept. probability reduces to the ratio of target distrbutions
+    # assuming a symmetric proposal distribution the accept. probability reduces to the ratio of target distrbutions
     ratio = target_distribution(x_proposed, data, prior_par, ...)/target_distribution(x[i], data, prior_par, ...)
-    #ratio <- dbeta(x_proposed, alpha_prior + nr_successes, beta_prior + nr_trials - nr_successes)/dbeta(x[i], alpha_prior + nr_successes, beta_prior + nr_trials - nr_successes)
-    
+
     # acceptance step
-    if(runif(1, 0, 1) < min(c(1, ratio))){
-      x[i+1] <- x_proposed
+    if(runif(1) < min(c(1, ratio))){
+      x[i+1,] <- x_proposed
       acc <- acc +1
     } else {
-      x[i+1] <- x[i]
+      x[i+1,] <- x[i,]
     }
     acceptance_ratio = acc/n_iter
   }
   return(list(x=x, accept_ratio=acceptance_ratio))
 }
+
+
