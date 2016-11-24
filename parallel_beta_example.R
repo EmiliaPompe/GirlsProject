1105839a2785282b7372f32a2ce04f75f0fe5916
Tamar Loach <tamar.loach@spc.ox.ac.uk>
2016-11-24 15:56:24 +0000

    Add profiling

--- parallel_beta_example.R
+++ parallel_beta_example.R
@@ -1,7 +1,5 @@
 library(parallel)
 
-Rprof('test_profile.out')
-
 observations <- c(1, rep(0, times=999))
 nr_servers <- 4
 shards <- split(observations, rep(seq_len(nr_servers),each=length(observations)/nr_servers))
@@ -20,9 +18,6 @@ clusterEvalQ(cl = clust, source("beta_example.R"))
 lambda <- clusterApplyLB(clust, shards, BetaMH, N=n_iter, sigma=sigma, alpha_prior=1/nr_servers, beta_prior=1/nr_servers)
 
 stopCluster(clust)
-
-Rprof()
-summaryRprof('test_profile.out')
 
 df = data.frame(lapply(lambda, function(y) y$x))
 colnames(df) <- paste0('x', seq_len(nr_servers))
