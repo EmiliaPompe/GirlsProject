00ca4892ad6c9477e3ab3194e9d8abc8468ce4e1
Virginia Aglietti <aglietti@toucanet.stats.ox.ac.uk>
2016-11-24 11:59:09 +0000

    inser number of iter and burn in the esamples

--- beta_example_plots.R
+++ beta_example_plots.R
@@ -4,27 +4,31 @@ source("beta_example.R")
 observations <- c(1, rep(0, times=999))
 
 n_iter = 10000
-result <- BetaMH(observations, n_iter, sigma =0.001)
-markov_chain <- result$x
-result$acc/n_iter
-plot(1000:10000, markov_chain[1000:10000], type="l")
-acf(markov_chain)
-hist(markov_chain[1000:10000], 50, col='blue')
-hist(rbeta(9000, 2, 1001), 50, col='red', alpha=0.9, add=TRUE)
-
-n_iter = 1000
 burn_in = 0.1*n_iter
-result <- BetaMH(observations, n_iter, sigma = 1)
+sigma = 0.001
+result = BetaMH(observations, n_iter, sigma =sigma)
+markov_chain = result$x
+result$acc/n_iter
+plot(burn_in:n_iter, markov_chain[burn_in:n_iter], type="l")
+acf(markov_chain)
+hist(markov_chain[burn_in:n_iter], 50, col='blue')
+hist(rbeta((n_iter-burn_in), 2, 1001), 50, col='red', alpha=0.9, add=TRUE)
+
+
+n_iter = 100000  #with higher value of sigma you need higher number of iterations in order for the hist to look as a Beta
+burn_in = 0.1*n_iter
+sigma = 1
+result = BetaMH(observations, n_iter, sigma = sigma)
 markov_chain = result$x
 markov_chain_with_burn_in = markov_chain[burn_in:n_iter]
 
 par(mfrow=c(1,2))
 hist(markov_chain_with_burn_in, 50, col='blue')
-hist(rbeta(90000, 2, 1000), 50, col='red')
+hist(rbeta((n_iter-burn_in), 2, 1000), 50, col='red')
+acf(markov_chain[burn_in:n_iter], main='ble2') #the acf will be higher for higher value of sigma
 
-acf(markov_chain[10000:1000000], main='ble2')
 
 par(mfrow=c(1,3))
-plot(density(markov_chain[10000:100000]), xlim=c(0,0.016))
-plot(density(rbeta(90000, 2, 1000)), col='red', add=TRUE, xlim=c(0,0.016))
-plot(density(rbeta(90000, 2, 1001)), col='red', add=TRUE, xlim=c(0,0.016))
+plot(density(markov_chain[burn_in:n_iter]), xlim=c(0,0.016))
+plot(density(rbeta((n_iter-burn_in), 2, 1000)), col='red', xlim=c(0,0.016))
+plot(density(rbeta((n_iter-burn_in), 2, 1001)), col='red', xlim=c(0,0.016))
