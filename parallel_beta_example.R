977f4e6441ee1e0da85805b3c372615dba75750a
Tamar Loach <tamar.loach@spc.ox.ac.uk>
2016-11-24 13:09:07 +0000

    Compare paralell example to single machine

--- parallel_beta_example.R
+++ parallel_beta_example.R
@@ -4,7 +4,7 @@ observations <- c(1, rep(0, times=999))
 nr_servers <- 4
 shards <- split(observations, rep(seq_len(nr_servers),each=length(observations)/nr_servers))
 
-n_iter = 100
+n_iter = 10000
 burn_in = 0.1*n_iter
 sigma = 0.001
 
@@ -24,3 +24,22 @@ df = data.frame(lapply(lambda, function(y) y$x))
 colnames(df) <- paste0('x', seq_len(nr_servers))
 
 df$mean = rowMeans(df)
+
+par(mfrow=c(1,1))
+result = BetaMH(observations, n_iter, sigma = sigma)
+markov_chain = result$x
+
+d1 = density(markov_chain)
+d2 = density(df$mean)
+d3 = density(rbeta(10000,2,1000))
+
+plot(range(d1$x, d2$x, d3$x), range(d1$y, d2$y, d3$y), type = "n", xlab = "x",
+     ylab = "Density", xlim=c(0,0.01))
+lines(d1, col = "red")
+lines(d2, col = "blue")
+lines(d3, col = "black")
+
+plot(density(markov_chain))
+plot(density(df$mean), col='red')
+
+qqplot(markov_chain[burn_in:n_iter], df$mean[burn_in:n_iter])
