580d43785bb2b6fb0c714a2b142524aa7934f06f
Tamar Loach <tamar.loach@spc.ox.ac.uk>
2016-11-25 12:34:11 +0000

    print out acceptance rate

--- ConsensusMCMC/src/gslrng.c
+++ ConsensusMCMC/src/gslrng.c
@@ -35,8 +35,8 @@ void BetaMH(long *restrict nP, double *restrict sigmaP, double *restrict alpha_p
   }
   
   long n, i;
-  double sigma, x, x_proposed, u, acc_prob, alpha_post, beta_post, alpha_prior, beta_prior;
-  int num_successes = 1, num_failures = 9;
+  double sigma, x, x_proposed, u, acc_prob, alpha_post, beta_post, alpha_prior, beta_prior, denom;
+  int num_successes = 1, num_failures = 999, acc_count = 0;
   
   alpha_prior = *alpha_priorP;
   beta_prior = *beta_priorP;
@@ -50,14 +50,24 @@ void BetaMH(long *restrict nP, double *restrict sigmaP, double *restrict alpha_p
   for (i=1; i<n; i++)
   {
     x_proposed = x + gsl_ran_gaussian(rP, sigma);
-    acc_prob = min(1.0, gsl_ran_beta_pdf(x_proposed, alpha_post, beta_post)/gsl_ran_beta_pdf(x, alpha_post, beta_post));
+    denom = gsl_ran_beta_pdf(x, alpha_post, beta_post);
+    if (denom != 0)
+    {
+      acc_prob = min(1.0, gsl_ran_beta_pdf(x_proposed, alpha_post, beta_post)/denom);
+    }
+    else{
+      acc_prob = 0;
+      printf("Divide by zero");
+    }
     u = gsl_ran_flat(rP,0.0,1.0);
     
     if (u < acc_prob)
     {
       x = x_proposed;
+      acc_count++;
     }
       
     vec_xP[i] = x;
   }
+  printf("Acceptance rate: %lf\n", (float) acc_count/n);
 }
