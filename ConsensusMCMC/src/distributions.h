57c9d0b9e5f3c73d19d48aa3946ce2e511580363
VirgiAgl <agliettivirginiauk@gmail.com>
2016-11-28 21:48:45 +0000

    rm files moved in Old folder

--- ConsensusMCMC/src/distributions.h
+++
@@ -1,5 +0,0 @@
-#ifndef DISTRIBUTIONS_H
-#define DISTRIBUTIONS_H
-double betaTargetDistribution(double *restrict xP, double *restrict dataP, int *restrict data_lenP, double *restrict alpha_priorP, double *restrict beta_priorP);
-double gammaTargetDistribution(double *restrict xP, int *restrict dataP, int *restrict data_lenP, double *restrict k_priorP, double *restrict theta_priorP);
-#endif
