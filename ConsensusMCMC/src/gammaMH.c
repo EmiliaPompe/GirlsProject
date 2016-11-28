5fabb527a0a41ef3d930c6567cc78b18a99f7f9d
VirgiAgl <agliettivirginiauk@gmail.com>
2016-11-28 21:21:37 +0000

    introduce the analytic expression for the Gamma target distribution. TO RUN!!!

--- ConsensusMCMC/src/gammaMH.c
+++ ConsensusMCMC/src/gammaMH.c
@@ -10,7 +10,7 @@ void gammaMH(int *restrict dataP, int *restrict data_lenP,  int *restrict nP, do
 {
   
   int n, i;
-  double sigma, x, x_proposed, u, acc_prob;
+  double sigma, x, x_proposed, u, acc_prob, prior_ratio, log_lik_difference, s;
   int acc_count;
   
   static gsl_rng *restrict rP = NULL;
@@ -24,12 +24,25 @@ void gammaMH(int *restrict dataP, int *restrict data_lenP,  int *restrict nP, do
 
   n = *nP;
   sigma = *sigmaP;
+  k_prior = *k_priorP;
+  theta_prior = *theta_priorP;
+  data_len = *data_lenP;
+  data = *dataP
+  s = (double) *sP;
+  
   x = 4.0;
   vec_xP[0] = x; // *(myPointer + index) and myPointer[index] are equivalent
   for (i=1; i<n; i++)
   {
     x_proposed = x + gsl_ran_gaussian(rP, sigma);  // random walk MH
-    acc_prob = min(1.0, gammaTargetDistribution_v2(&x_proposed,  dataP,  data_lenP,  k_priorP, theta_priorP, sP)/gammaTargetDistribution_v2(&x,  dataP,  data_lenP,  k_priorP, theta_priorP, sP));
+    
+    prior_ratio = pow((pow(x_proposed, k_prior-1)*exp(-x_proposed/theta_prior))/(pow(x, k_prior-1)*exp(-x/theta_prior)), (1.0/s))
+    
+    log_lik_difference = (-data_len*x_proposed) + ((log(x_proposed))*sum(data)) + (data_len*x) - ((log(x))*sum(data))
+    
+    acc_prob = min(1.0, prior_ratio * exp(log_lik_difference));
+    
+    //acc_prob = min(1.0, gammaTargetDistribution_v2(&x_proposed,  dataP,  data_lenP,  k_priorP, theta_priorP, sP)/gammaTargetDistribution_v2(&x,  dataP,  data_lenP,  k_priorP, theta_priorP, sP));
     u = gsl_ran_flat(rP,0.0,1.0);
     if (u < acc_prob)
     {
