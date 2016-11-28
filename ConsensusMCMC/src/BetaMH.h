9395fc85f679e826c76e35de7408cb7ee956c5d4
VirgiAgl <agliettivirginiauk@gmail.com>
2016-11-28 21:43:10 +0000

    rm files moved in Old folder

--- ConsensusMCMC/src/BetaMH.h
+++
@@ -1,5 +0,0 @@
-#ifndef BETAMH_H
-#define BETAMH_H
-void BetaMH(int *restrict nP, double *restrict sigmaP, double *restrict alpha_priorP, double *restrict beta_priorP, double *restrict vec_xP);
-#endif
-  
