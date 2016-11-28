8b3012f271b147d397a56881f5aaba559679e0b7
VirgiAgl <agliettivirginiauk@gmail.com>
2016-11-28 21:35:51 +0000

    move old c functions in a folder

---
+++ ConsensusMCMC/src/OLD_C_functions/BetaMH.h
@@ -0,0 +1,5 @@
+#ifndef BETAMH_H
+#define BETAMH_H
+void BetaMH(int *restrict nP, double *restrict sigmaP, double *restrict alpha_priorP, double *restrict beta_priorP, double *restrict vec_xP);
+#endif
+  
