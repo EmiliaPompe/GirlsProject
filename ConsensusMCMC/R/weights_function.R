39c970215fa0d4991ff39fbec09fea20f9fc57e7
Virginia Aglietti <aglietti@toucanet.stats.ox.ac.uk>
2016-11-29 16:34:57 +0000

    add documentation for weights computation function

--- ConsensusMCMC/R/weights_function.R
+++ ConsensusMCMC/R/weights_function.R
@@ -1,4 +1,15 @@
 
+#' Weights computation for Consensus MCMC algorithm
+#' 
+#' \code{} The function computes the weights used for the Consensus MCMC algorithm. 
+#' The computed weights are used to aggregate the Markov Chains obtained parallelizing the data on different machines. 
+#' @param x A number
+#' @param y A number
+#' @return The sum of \code{x} and \code{y}
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+
 ### function to compute weights to aggregate parallel MCMC (constant weights or sample variance) 
 
 weightsComputation = function(df, method){
@@ -18,12 +29,3 @@ weightsComputation = function(df, method){
   return(result)
 }
 
-# df = data.frame(lapply(lambda, function(y) y))
-# colnames(df) <- paste0('x', seq_len(nr_servers))
-# 
-# machine_sample_variance = apply(df, 2, var)
-# machine_precision = 1/machine_sample_variance
-# 
-# sum_machine_precision = sum(machine_precision)
-# 
-# parallel_chain = (as.matrix(df) %*% machine_precision)/sum_machine_precision
