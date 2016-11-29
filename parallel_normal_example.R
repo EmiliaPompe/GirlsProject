eed648792137f66f9439800a5c8677c7760e9d57
Virginia Aglietti <aglietti@toucanet.stats.ox.ac.uk>
2016-11-29 12:07:50 +0000

    modify plots for the normal plots

--- parallel_normal_example.R
+++ parallel_normal_example.R
@@ -1,14 +1,19 @@
 library(parallel)
+library(devtools)
+library(ggplot2)
 source("plotting_functions.R")
 
-sigma_known = 0.5
-observations <- rnorm(10000, 0 , sigma_known)
+sigma_known = 0.1
+observations <- rnorm(1000, 0 , sigma_known)
 nr_servers <- 4
 shards <- split(observations, rep(seq_len(nr_servers),each=length(observations)/nr_servers))
 
-n_iter = 10000
+n_iter = 1000
 burn_in = 0.1*n_iter
 sigma = 0.01
+mean_prior=0
+sigma_prior=1.0
+x_0 = 0
 
 clust <- makePSOCKcluster(names = c("greywagtail",
                                     "greyheron",
@@ -17,7 +22,7 @@ clust <- makePSOCKcluster(names = c("greywagtail",
 
 clusterEvalQ(cl = clust, devtools::load_all("~/Workspace/GirlsProject/ConsensusMCMC/"))
 
-lambda <- clusterApplyLB(clust, shards, NormalMH, n=n_iter, sigma=sigma, mean_prior=0, sigma_prior=1.0, sigma_known=sigma_known, s= nr_servers)
+lambda <- clusterApplyLB(clust, shards, NormalMH, n=n_iter, sigma=sigma, mean_prior=mean_prior, sigma_prior=sigma_prior, sigma_known=sigma_known, s= nr_servers, x_0 = x_0)
 
 stopCluster(clust)
 
@@ -27,21 +32,27 @@ colnames(df) <- paste0('x', seq_len(nr_servers))
 df$mean = rowMeans(df)
 
 par(mfrow=c(1,1))
-result = NormalMH(observations, n = n_iter, sigma = sigma, mean_prior=1/nr_servers, sigma_prior=1/nr_servers, sigma_known=sigma_known, s=1, x_0 = 1) #are these args right TODO
+result = NormalMH(observations, n = n_iter, sigma = sigma, mean_prior=mean_prior, sigma_prior=sigma_prior, sigma_known=sigma_known, s=1, x_0 = x_0) #are these args right TODO
 markov_chain = result
-HistPlot(list(markov_chain))
+mean_post = (mean_prior/sigma_prior^2 + sum(observations)/sigma_known^2)/(1/sigma_prior^2 + length(observations)/sigma_known^2)
+sigma_post = sqrt(1/(1/sigma_prior^2 + length(observations)/sigma_known^2))
 
-d1 = density(markov_chain)
-d2 = density(df$mean)
-d3 = density(rnorm(1000, 4 , sigma_known))
+HistPlot(list(markov_chain, rnorm(1000, mean_post, sigma_post)), method = c("markov chain", "theor"), burn_in = 0.3)
 
-plot(range(d1$x, d2$x, d3$x), range(d1$y, d2$y, d3$y), type = "n", xlab = "x",
-     ylab = "Density", xlim=c(0,0.01))
-lines(d1, col = "red")
-lines(d2, col = "blue")
-lines(d3, col = "black")
+HistPlot(list(markov_chain, df$mean, rnorm(1000, mean_post, sigma_post)), method = c("One machine", "4 machines", "theoretical post distribution"), burn_in = 0.3)
 
-plot(density(markov_chain))
-plot(density(df$mean), col='red')
+HistPlot(list(df$x1, df$x2, df$x3, df$x4), method = c("1", "2", "3", "4"))
+#d1 = density(markov_chain)
+#d2 = density(df$mean)
+#d3 = density(rnorm(1000, mean_post, sigma_post))
+
+#plot(range(d1$x, d2$x, d3$x), range(d1$y, d2$y, d3$y), type = "n", xlab = "x",
+#     ylab = "Density", xlim=c(0,0.01))
+#lines(d1, col = "red")
+#lines(d2, col = "blue")
+#lines(d3, col = "black")
+
+#plot(density(markov_chain))
+#plot(density(df$mean), col='red')
 
 qqplot(markov_chain[burn_in:n_iter], df$mean[burn_in:n_iter])
