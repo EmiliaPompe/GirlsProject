6ea9dde763cd754e45095a8b0b8555542630be0b
Virginia Aglietti <aglietti@toucanet.stats.ox.ac.uk>
2016-11-30 19:08:45 +0000

    add APi

--- run_amazon.R
+++ run_amazon.R
@@ -10,7 +10,6 @@ x_0 = 0.0
 
 n_iter_vec = c(1000, 10000, 100000, 1000000)
 n_machines = c(1,4,10)
-#time_vec = matrix(NA, nrow=n_alternatives , ncol=4)
 time_running_one_core = matrix(NA, nrow=3, ncol=4)
 
 for (k in seq_along(n_iter_vec)){
@@ -19,9 +18,29 @@ for (k in seq_along(n_iter_vec)){
     nr_iter = n_iter_vec[k]
     shards <- split(observations, rep(seq_len(nr_servers),each=length(observations)/nr_servers))
     
-    #clust <- makePSOCKcluster(names = c("greywagtail"))# change it!
-    #clusterEvalQ(cl = clust, devtools::load_all("~/ConsensusMCMC"))
-      time_running[m, k] = unname(system.time({
+    if (nr_servers = 1){
+      clust <- makePSOCKcluster(names = c("52.53.199.26"))
+      clusterEvalQ(cl = clust, devtools::load_all("~/ConsensusMCMC"))
+      
+    }
+    
+    if (nr_servers = 4){
+      clust <- makePSOCKcluster(names = c("52.53.199.26", "54.153.29.6",  
+                                          "52.53.225.3", "54.183.194.139"))
+      clusterEvalQ(cl = clust, devtools::load_all("~/ConsensusMCMC"))
+      
+    }
+    
+    if (nr_servers = 10){
+      clust <- makePSOCKcluster(names = c("52.53.199.26", "54.153.29.6",  
+                                          "52.53.225.3", "54.183.194.139",
+                                          "54.183.166.57", "54.183.73.68", 
+                                          "54.153.96.150", "54.67.35.63", 
+                                          "54.193.80.240", "52.53.220.121"))
+      clusterEvalQ(cl = clust, devtools::load_all("~/ConsensusMCMC"))
+      
+    }
+    time_running_one_core[m, k] = unname(system.time({
       lambda <- clusterApplyLB(clust, shards, NormalMultiCoreMH, multicore=0, n=n_iter, sigma=sigma, mean_prior=mean_prior, 
                                sigma_prior=sigma_prior, sigma_known=sigma_known, s= nr_servers, x_0 = x_0, num_cores=1)
    })[3])
@@ -34,8 +53,7 @@ for (k in seq_along(n_iter_vec)){
 n_iter_vec = c(1000, 10000, 100000, 1000000)
 n_cores = c(1, 4, 8 )
 
-#time_vec = matrix(NA, nrow=n_alternatives , ncol=4)
-time_running_diff_machines = matrix(NA, nrow=3, ncol=4)
+time_running_diff_cores = matrix(NA, nrow=3, ncol=4)
 
 for (k in seq_along(n_iter_vec)){
   for (m in seq_along(n_cores)){
@@ -44,9 +62,14 @@ for (k in seq_along(n_iter_vec)){
     nr_iter = n_iter_vec[k]
     shards <- split(observations, rep(seq_len(nr_servers),each=length(observations)/nr_servers))
     
-    #clust <- makePSOCKcluster(names = c("greywagtail"))# change it!
-    #clusterEvalQ(cl = clust, devtools::load_all("~/ConsensusMCMC"))
-    time_running[m, k] = unname(system.time({
+    clust <- makePSOCKcluster(names = c("52.53.199.26", "54.153.29.6",  
+                                        "52.53.225.3", "54.183.194.139",
+                                        "54.183.166.57", "54.183.73.68", 
+                                        "54.153.96.150", "54.67.35.63", 
+                                        "54.193.80.240", "52.53.220.121")) 
+    clusterEvalQ(cl = clust, devtools::load_all("~/ConsensusMCMC"))
+    
+    time_running_diff_cores[m, k] = unname(system.time({
       lambda <- clusterApplyLB(clust, shards, NormalMultiCoreMH, multicore=1, n=n_iter, sigma=sigma, mean_prior=mean_prior, 
                                sigma_prior=sigma_prior, sigma_known=sigma_known, s= nr_servers, x_0 = x_0, num_cores=n_cores[m])
     })[3])
