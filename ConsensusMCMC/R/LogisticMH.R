a14b74e0093d9274dba7476024abb9b52d5f1abd
Emilia Pompe <pompe@tocotoucan.stats.ox.ac.uk>
2016-12-01 09:42:42 +0000

    now the output of Logistic MH is a data frame

--- ConsensusMCMC/R/LogisticMH.R
+++ ConsensusMCMC/R/LogisticMH.R
@@ -5,6 +5,9 @@ LogisticMH <- function(data_frame,  n_iter, sigma, sigma_prior, mean_prior, s, x
   ans <- .C("LogisticMH", as.integer(data), as.vector(as.double(t(z))), as.integer(length(data)), 
             as.integer(ncol(z)), as.integer(n_iter), as.double(sigma), as.double(sigma_prior), 
             as.double(mean_prior), as.integer(s), as.double(x_0),  vec_xP=as.double(rep(0, (n_iter+1)*(ncol(z)))))
-  return(ans$vec_xP)
+  
+  result <- as.data.frame(matrix(ans$vec_xP, ncol = ncol(z), byrow=TRUE))
+  colnames(result) <- paste0('beta_', seq_len(result)-1)
+  return(result)
 }
 
