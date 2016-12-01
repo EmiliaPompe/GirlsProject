9bff070ddc0ef5a565a9655042eb3cca2a35f8fc
Virginia Aglietti <aglietti@toucanet.stats.ox.ac.uk>
2016-12-01 11:21:59 +0000

    update documentations

--- ConsensusMCMC/R/weights_function.R
+++ ConsensusMCMC/R/weights_function.R
@@ -1,14 +1,46 @@
 
-#' Weights computation for Consensus MCMC algorithm
+#' Weights computation for Consensus MCMC algorithm targeting univariate distributions
 #' 
-#' \code{} The function computes the weights used for the Consensus MCMC algorithm. 
+#' \code{} The function computes the weights used for the Consensus MCMC algorithm targeting univariate distributions. 
 #' The computed weights are used to aggregate the Markov Chains obtained parallelizing the data on different machines. 
-#' @param df A dataframe derived from running \code{\link{BetaMH}} or \code{\link{NormalMH}} or \code{\link{GammaMH}} on the relative data. 
+#' @param df A dataframe derived from running \code{\link{BetaMH}} or \code{\link{NormalMH}} or \code{\link{GammaMH}} or \code{\link{NormalMultiCoreMH}}on the relative data. 
 #' @param method The method that needs to be used to aggregate the markov chains obtained from different machines. Need to be used when running in parallel. 
 #' @return The functions returns the aggregated Markov chain from the Consensus MCMC.  
 #' @examples
 #' 
+#' # Generate the data
+#' sigma_known = 1
+#' observations <- rnorm(10000, 3 , sigma_known)
+#' nr_servers <- 4
+#' 
+#' 
+#' # Set the parameters
+#' n_iter = 10000
+#' n_iter = 10000
+#' burn_in = 0.1*n_iter
+#' sigma = 0.01  # sigma for the proposal distribution
+#' mean_prior=0
+#' sigma_prior=1.0
+#' x_0 = -5
+#' 
+#' # Run the Consensus MCMC algorithm on different machines
+#' #' shards <- split(observations, rep(seq_len(nr_servers),each=length(observations)/nr_servers))
+#' clust <- makePSOCKcluster(names = c("greywagtail",
+#'                                    "greyheron",
+#'                                    "greypartridge",
+#'                                    "greyplover"))
+#'                                    
+#' clusterEvalQ(cl = clust, devtools::load_all("~/Workspace/GirlsProject/ConsensusMCMC/"))
+#'
+#' lambda <- clusterApplyLB(clust, shards, NormalMH, n=n_iter, sigma=sigma, mean_prior=mean_prior, sigma_prior=sigma_prior, sigma_known=sigma_known, s= nr_servers, x_0 = x_0)
+#'
+#' stopCluster(clust)
+#'
 #' df = data.frame(lapply(lambda, function(y) y))
+#' colnames(df) <- paste0('x', seq_len(nr_servers))
+#' df = data.frame(lapply(lambda, function(y) y))
+#' 
+#' # Aggregate the Markov chains
 #' parallel_markov_chain = weightsComputation(df, method = "sample variance")
 #' 
 
--- ConsensusMCMC/R/weights_multivariate_function.R
+++ ConsensusMCMC/R/weights_multivariate_function.R
@@ -1,3 +1,46 @@
+
+#' Weights computation for Consensus MCMC algorithm targeting multivariate distributions
+#' 
+#' \code{} The function computes the weights used for the Consensus MCMC algorithm targeting multivariate distributions. 
+#' The computed weights are used to aggregate the Markov Chains obtained parallelizing the data on different machines. 
+#' @param result_list A list derived from running \code{\link{LogisticMH}} on the relative data. 
+#' @param method The method that needs to be used to aggregate the markov chains obtained from different machines. Need to be used when running in parallel. 
+#' @return The functions returns the aggregated Markov chain from the Consensus MCMC.  
+#' @examples
+#' 
+#' # Load the data frame
+#' # not run
+#' #load('logistic_data_frame.RData')
+#' 
+#' # Set the parameters
+#' sigma = c(0.1, 0.1, 0.1, 0.1, 0.1)           
+#' sigma_prior = c(1,1,1,1,1)                   
+#' mean_prior = c(-2, 0.5, 0, 0.5, 2)           
+#' x_0 = c(-3.5,1.7,-0.2,0.5,3.5)               
+#' nr_servers = 4                    
+#' n_iter = 10000
+#' burn_in = 0.2
+#' 
+#' # Run the Consensus MCMC algorithm on different machines
+#' shards <- split(sample(1:nrow(logistic_data_frame),nrow(logistic_data_frame), replace = FALSE),
+#' rep(seq_len(nr_servers),times = nrow(logistic_data_frame)/nr_servers))
+#' shards = lapply(shards, function(x) logistic_data_frame[x,])
+#'
+#' clust <- makePSOCKcluster(names = c("greywagtail",
+#'                                    "greyheron",
+#'                                    "greypartridge",
+#'                                    "greyplover"))
+#'                                    
+#' clusterEvalQ(cl = clust, devtools::load_all("~/Workspace/GirlsProject/ConsensusMCMC/"))
+#'
+#' lambda <- clusterApplyLB(clust, shards, LogisticMH, n_iter=n_iter, sigma=sigma, mean_prior=mean_prior, sigma_prior=sigma_prior, s= nr_servers, x_0 = x_0)
+#'
+#' stopCluster(clust)
+#' 
+#' # Aggregate the Markov chains
+#' parallel_markov_chain = weightsMultivariateComputation(lambda, method="sample variance")
+#' 
+
 weightsMultivariateComputation <- function(result_list, method){
   nr_params <- ncol(result_list[[1]]) # we assume that all elements of list have the same number of columns
   
